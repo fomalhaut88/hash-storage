ceba3a4d1158d2f46d2db12d492cb7b25cbd805e
alexander-rf <a.khlebushhev@robo.cash>
2020-05-25 18:28:52 +0300

    Update restart.sh

--- restart-example.sh
+++ restart-example.sh
@@ -1,3 +1,3 @@
-docker stop hash-storage
+docker stop hash-storage-app
 docker build -t hash-storage --build-arg DATABASE_URL=mysql://hash_storage:gooriruc2Al4eeSh@172.17.0.1:3306/hash_storage .
-docker run -it -p 8000:8080 --name hash-storage-app --restart=always -d hash-storage
+docker run -it -p 8000:8000 --name hash-storage-app --restart=always -d hash-storage
