a25fd3abccb781f057effe13001d695c0e27e65d
Хлебущев Александр Сергеевич <a.khlebushhev@robo.cash>
2020-07-31 15:39:19 +0300

    restart.sh

--- restart-example.sh
+++ restart-example.sh
@@ -1,4 +1,4 @@
 docker stop hash-storage-app
 docker rm hash-storage-app
-docker build -t hash-storage --build-arg DATABASE_URL=mysql://hash_storage:gooriruc2Al4eeSh@172.17.0.1:3306/hash_storage .
-docker run -it -p 8000:8000 --name hash-storage-app --restart=always -d hash-storage
+docker build -t hash-storage --build-arg DATABASE_URL=/usr/src/app/tmp/sqlite.db .
+docker run -it -p 8000:8000 --name hash-storage-app --volume /path/to/database/folder:/usr/src/app/tmp --restart=always -d hash-storage
